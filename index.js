20913ced52917c5ae0379336509d870111a1084f
felixp83 <felixp8@web.de>
2025-06-23 16:59:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,21 +15,4 @@ app.get('/', (req, res) => {
 
   // Hintergrund
   ctx.fillStyle = '#222';
-  ctx.fillRect(0, 0, width, height);
-
-  // Text
-  ctx.fillStyle = '#ffffff';
-  ctx.font = 'bold 40px Sans';
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.fillText(text, width / 2, height / 2);
-
-  // Header für PNG
-  res.setHeader('Content-Type', 'image/png');
-
-  // Bild zurücksenden
-  canvas.createPNGStream().pipe(res);
-});
-
-app.listen(port, () => {
-  cons
+  ctx.fillRect(0, 0, widt
