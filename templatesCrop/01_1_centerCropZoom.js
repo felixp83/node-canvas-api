71748a504aed33a57edaae0ed57333ad117287b2
felixp83 <felixp8@web.de>
2025-10-16 17:28:58 +0200

    Update 01_1_centerCropZoom.js

--- templatesCrop/01_1_centerCropZoom.js
+++ templatesCrop/01_1_centerCropZoom.js
@@ -6,8 +6,8 @@ module.exports = async function centerCropWithDynamicZoom(img) {
   const targetRatio = targetWidth / targetHeight; // ≈0.6667
 
   // Dynamischer Zoomfaktor zwischen 1.10 (110%) und 1.20 (120%)
-  const minZoom = 1.18;
-  const maxZoom = 1.28;
+  const minZoom = 1.12;
+  const maxZoom = 1.22;
   const zoomFactor = Math.random() * (maxZoom - minZoom) + minZoom;
 
   const imgWidth = img.width;
